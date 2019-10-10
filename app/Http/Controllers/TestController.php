cd8bfd894d75479418822fc7041cc8b667a71aec
stefi7773 <stefi7773@gmail.com>
2019-10-10 18:06:45 +0200

    Initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class TestController extends Controller
+{
+    public function test1()
+    {
+        echo "Hello World!<br>";
+    }
+
+    public function test2()
+    {
+        $this->test1();
+        for($i = 0; $i < 10; $i++)
+        {
+            echo "Cislo: " . $i . "<br>";
+        }
+        return true;
+    }
+    public function test3()
+    {
+        if($this->test2()==true)
+        {
+            echo "End.";
+        }
+    }
+}
