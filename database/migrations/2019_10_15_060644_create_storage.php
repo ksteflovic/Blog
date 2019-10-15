0c2d45ee53cc5606c4a445bbafa28dfbcad59f4e
stefi7773 <stefi7773@gmail.com>
2019-10-15 08:22:31 +0200

    Database

--- database/migrations/2019_10_15_060644_create_storage.php
+++ database/migrations/2019_10_15_060644_create_storage.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStorage extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('storage', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('meno');
-            $table->string('priezvisko');
-            $table->string('email')->unique();
-            $table->string('heslo', 60);
+            $table->string('storage_type');
+            $table->string('storage_name');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('storage');
     }
 }
