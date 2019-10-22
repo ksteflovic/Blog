5995274bf6f95805c346938c17ba7068850ebe92
stefi7773 <stefi7773@gmail.com>
2019-10-22 10:41:41 +0200

    Bootstrap changes

--- resources/views/adduser.blade.php
+++ resources/views/adduser.blade.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<form method="post" action="{{ action('UserController@insertAction') }}">
-    <br>
-    <div class="input-group">
-        <div class="input-group-prepend">
-            <span class="input-group-text" id="">Meno a priezvisko</span>
-        </div>
-        <input type="text" name="firstname" value="Mickey" class="form-control">
-        <input type="text" name="firstname" value="Mouse" class="form-control">
-    </div>
-    Email: <br>
-    <div class="input-group mb-3">
-        <div class="input-group-prepend">
-            <span class="input-group-text" id="basic-addon1">@</span>
-        </div>
-        <input type="email" class="form-control" placeholder="E-mail" aria-label="email" aria-describedby="basic-addon1" name="email" value="@">
-    </div>
+<div>
+    <div style="float: left; width: 50%; margin: 20px;">
+        <form method="post" action="{{ action('UserController@insertAction') }}">
+            <br>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="">Meno a priezvisko</span>
+                </div>
+                <input type="text" name="firstname" value="Mickey" class="form-control">
+                <input type="text" name="lastname" value="Mouse" class="form-control">
+            </div>
+            <br>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">Email: </span>
+                </div>
+                <input type="email" class="form-control" placeholder="E-mail" aria-label="email"
+                       aria-describedby="basic-addon1" name="email" value="@">
+            </div>
 
-    <br>
-    Dátum narodenia: <br>
-    <input type="date" name="birthday" value="21.12.2012">
-    <br><br>
-    <input type="submit" value="Odoslať">
-</form>
+            <br>
+            Dátum narodenia: <br>
+            <input type="date" name="birthday" class="form-control" value="1986-05-11" min="1950-01-01" max="2019-12-31">
+            <br><br>
+            <div style="text-align: center">
+                <input type="submit" value="Odoslať" class="btn btn-primary">
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
