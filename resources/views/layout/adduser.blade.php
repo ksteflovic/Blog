9362659a0d7780a160d5a320340e5cb9f9f93580
stefi7773 <stefi7773@gmail.com>
2019-11-01 17:06:43 +0100

    GUI

--- resources/views/layout/adduser.blade.php
+++ resources/views/layout/adduser.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{url("../assets/css/style.css")}}" />
-</head>
-<body>
+@include('head');
+@include('navigate');
 <div>
     <div id="parent">
         <form method="post" action="{{ action('UserController@insertAction') }}" id="form_login">
---
+++ resources/views/navigate.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-dark bg-dark">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Create</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Insert</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Edit</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Delete</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Show</a>
+        </li>
+    </ul>
+</nav>
