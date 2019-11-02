309153e774c66df20d920d07e69cae31329d37ba
stefi7773 <stefi7773@gmail.com>
2019-11-02 12:51:34 +0100

    Delete action.

--- resources/views/crud_operations/delete.blade.php
+++ resources/views/crud_operations/delete.blade.php
@@ -3,15 +3,16 @@
 
 <div class="jumbotron">
     <h1 class="display-4">Vymazanie údajov z tabuľky</h1>
+    <br>
     <p class="lead">Určite chcete vymazať nasledujúce údaje z tabuľky?</p>
     <p><strong>Meno: </strong></p>
     <p><strong>Priezvisko: </strong></p>
     <p><strong>Email: </strong></p>
-    <p><strong>D8tum narodenia: </strong></p>
+    <p><strong>Dátum narodenia: </strong></p>
     <hr class="my-4">
     <p>Táto akcia je nevratná!</p>
     <p class="lead">
-        <a class="btn btn-danger btn-sm" role="button">Áno, vymazať</a>
+        <a class="btn btn-danger btn-bg" role="button">Áno, vymazať</a>
     </p>
 </div>
 
